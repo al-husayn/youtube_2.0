d20501c84a3bcd63375bf3c445e49bcca7c162f7
Al-Hussein Abubakar <82461094+al-husayn@users.noreply.github.com>
2022-08-30 12:55:42 +0000

    NavBar created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import {logo} from '../utils/constant'
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
-    <div>
-      NavBar
-    </div>
-  )
-}
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        top: 0,
+        justifyContent: "space-between",
+        background: "#C8A2C8",
+      }}>
+      <Link to={"/"} style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
+};
 
-export default NavBar
+export default NavBar;
---
+++ src/utils/constant.js
@@ -0,0 +1,46 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo =
+  "https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_youtube-512.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "AL Drake", icon: <CodeIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl =
+  "https://i.ytimg.com/an_webp/7u5VeZgKrB0/mqdefault_6s.webp?du=3000&sqp=CO7psZgG&rs=AOn4CLCtxcq5m-fQVOmUUb3HhCuIAbybkA";
+export const demoChannelUrl = "/channel/UCc19yVrMKZ9tCy40hWEc3BA";
+export const demoVideoUrl = "/video/7u5VeZgKrB0";
+export const demoChannelTitle = "AL Drake";
+export const demoVideoTitle =
+  "Free Resources or Websites To Learn Programming or CodingI";
+export const demoProfilePicture =
+  "https://yt3.ggpht.com/ytc/AMLnZu9aNdi9Sn5gyTW3775DemoVQXCQPLJi4ei80ICqXA=s176-c-k-c0x00ffffff-no-rj";
